1603bd374b6e8543a4934c4065996f45bf4073ab
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-08-15 20:39:05 +0200

    get model data with api

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,15 +2,14 @@
 import json
 from django.http import JsonResponse
 
+from products.models import Product
+
 def api_home(request, *args, **kwargs):
-    body = request.body
-    #gettting data in a python dic
+    #get model data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        data['title'] = model_data.title
+        data['description'] = model_data.description
+        data['price'] = model_data.price
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 #endpoint = "https://httpbin.org/status/200/"
 endpoint = "http://localhost:8000/api"
 #http request
-get_response = requests.get(endpoint, params={"query":"test"})
+get_response = requests.get(endpoint, json={"product":"123"})
 #print(get_response.text)
 print(get_response.json())
 #print(get_response.status_code)
