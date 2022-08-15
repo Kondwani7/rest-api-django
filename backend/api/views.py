0f5117075a7c6d820e2ff46f0812a84949713c6c
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-08-15 20:54:01 +0200

    api_views with rest_framework package

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,17 @@
 #get views 
 import json
-from django.http import JsonResponse
-
+from django.http import JsonResponse, HttpResponse
 from products.models import Product
+from django.forms.models import model_to_dict
+#api views
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     #get model data
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['title'] = model_data.title
-        data['description'] = model_data.description
-        data['price'] = model_data.price
-    return JsonResponse(data)
+        data = model_to_dict(model_data, fields=['title', 'price'])
+    return Response(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,6 +4,7 @@ import requests
 endpoint = "http://localhost:8000/api"
 #http request
 get_response = requests.get(endpoint, json={"product":"123"})
-#print(get_response.text)
-print(get_response.json())
+print(get_response.headers)
+print(get_response.text)
+#print(get_response.json())
 #print(get_response.status_code)
